a3feeb4ef74864254d4520cf30e0bf6584d96956
rohank7599 <rohankarade3699@gmail.com>
2025-03-15 13:24:20 +0530

    3rd commit

---
+++ src/Gitmethos/newDemoclass.java
@@ -0,0 +1,16 @@
+package Gitmethos;
+
+public class newDemoclass {
+	
+	
+	public void xyz()
+	{
+		System.out.println("checking");
+	}
+
+	public static void main(String[] args) {
+		newDemoclass nw = new newDemoclass();
+		nw.xyz();
+		
+	}
+}
